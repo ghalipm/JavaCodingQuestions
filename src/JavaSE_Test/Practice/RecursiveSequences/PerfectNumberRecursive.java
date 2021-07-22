f9394db69265103e0ca3ca0441b8c4e80ffb7678
ghalipm(Ghalip Muhammed -  Alip Mohammed - Maimaiti Alifu ) <ghalipm@gmail.com>
2021-07-22 11:22:42 -0400

    recursive example

--- src/JavaSE_Test/Practice/RecursiveSequences/PerfectNumberRecursive.java
+++ src/JavaSE_Test/Practice/RecursiveSequences/PerfectNumberRecursive.java
@@ -1,4 +1,4 @@
-package JavaSE_Test.Practice;
+package JavaSE_Test.Practice.RecursiveSequences;
 
 /**
  * Bakhtiar Semseddin  11:36 AM
@@ -36,19 +36,21 @@ public class PerfectNumberRecursive {
         return squareSum;
     }
 
+    //My observation:
     //int[] attractors={1, 4};
-
     // determines if number n is perfect or not
-    // the sum of squares of digits is 1, then it is perfect;
-    // if the sum of squares of digits is 4, then it is not
-    //if the sum of squares of digits is neither 1 nor 4,
-    // then it should keep calculating using the sum of squares of digits as new input
+    // If the sum of squares of digits is 1, then it is perfect;
+    // if the sum of squares of digits is 4, then it is not perfect;
+    //If the sum of squares of digits is neither 1 nor 4,
+    // then it should keep calculating using
+    // the sum of squares of digits as the new input
+    // until it reaches either 1=1*1 or 4=2*2.
     public static boolean isPerfectNumber(int n){
         int newNum=0;
-        System.out.println("digitSquareSumFinder(n) = " + digitSquareSumFinder(n));
+        //System.out.println("digitSquareSumFinder(n) = " + digitSquareSumFinder(n));
         if(digitSquareSumFinder(n)==1) {return true;}
         else if(digitSquareSumFinder(n)==4){
-            System.out.println("The number is NOT a perfect number!");
+            //System.out.println("The number is NOT a perfect number!");
             return false;
         }else{
           return isPerfectNumber(digitSquareSumFinder(n));
@@ -57,10 +59,21 @@ public class PerfectNumberRecursive {
         }
 
     public static void main(String[] args) {
-        int n=29; // n=2,3,4,5,6,8,9,11,12,14,15,16,17,18,20;   1, 7, 13, 19; 23,28;
+        int n=100; // n=2,3,4,5,6,8,9,11,12,14,15,16,17,18,20;   1, 7, 13, 19; 23,28; 31,32;44,49;
         System.out.println("Original number n =: " + n);
-        System.out.println("isPerfectNumber("+n+") = " + isPerfectNumber(n));
+        //System.out.println("isPerfectNumber("+n+") = " + isPerfectNumber(n));
+        int count=0;
 
+        for (int i = 1; i <=n ; i++) {
+            if(isPerfectNumber(i)==true) {
+                //System.out.println("isPerfectNumber(" + i + ") = " + isPerfectNumber(i));
+                System.out.print(i+",");
+                count++;
+            }
+        }
+        System.out.println("=================");
+        System.out.println("count = " + count);
+        System.out.println("density="+ (double)count/n);
 
     }
 
