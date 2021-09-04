6bb43ab59026983fbd09bc41359104c4fc1a8a05
ghalipm(Ghalip Muhammed -  Alip Mohammed - Maimaiti Alifu ) <ghalipm@gmail.com>
2021-09-04 11:50:51 -0400

    a real interview question: workout

--- src/JavaSE_Test/UEN_Practice/TestResults.java
+++ src/JavaSE_Test/UEN_Practice/TestResults.java
@@ -8,8 +8,8 @@ import java.util.stream.Stream;
 public class TestResults
 {
     public static class Student {
-        private String name;
-        private int score;
+        private final String name;
+        private final int score;
         public Student(String name, int score) {
             this.name = name;
             this.score = score;
@@ -26,18 +26,26 @@ public class TestResults
                 students.filter(p-> p.score>=passingScore).forEach(passingStudents::add);
 
                 List<String> passingStudentsList=new ArrayList<>();
-        for (int i = 0; i <passingStudents.size() ; i++) {
-            passingStudentsList.add("Name: "+passingStudents.get(i).getName()+", Score: "+passingStudents.get(i).getScore());
+        /*
+                for (int i = 0; i <passingStudents.size() ; i++) {
+                passingStudentsList.add("Name: "+passingStudents.get(i).getName()+", Score: "+passingStudents.get(i).getScore());
+                }
+
+         */
+
+        for (Student passingStudent : passingStudents) {
+            passingStudentsList.add("Name: " + passingStudent.getName() + ", Score: " + passingStudent.getScore());
         }
 
 
 return passingStudentsList;
     }
+
     public static void main(String[] args) {
         List<Student> students = new ArrayList<>();
-        students.add(new Student("“Mike”", 80));
-        students.add(new Student("“James”", 57));
-        students.add(new Student("“Alan”", 21));
+        students.add(new Student("Mike", 80));
+        students.add(new Student("James", 57));
+        students.add(new Student("Alan", 21));
         studentsThatPass(students.stream(), 50).forEach(System.out::println);
     }
 }
