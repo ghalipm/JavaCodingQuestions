1cfa926ff6548dcac69a34f7938d9296c5f22a36
ghalipm(Ghalip Muhammed -  Alip Mohammed - Maimaiti Alifu ) <ghalipm@gmail.com>
2021-09-17 00:07:45 -0400

    practice stream

--- src/JavaSE_Test/Practice/Stream/MyStreamPractice.java
+++ src/JavaSE_Test/Practice/Stream/MyStreamPractice.java
@@ -8,10 +8,13 @@ import java.util.stream.Collectors;
 public class MyStreamPractice {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        System.out.println("list = " + list);
 
         // double the elements of the list.
         List<Integer> listTimes2 = list.stream().map( a ->  a*2  ).collect(Collectors.toList());
-        System.out.println("list = " + listTimes2);
+        System.out.println("listTimes2 = " + listTimes2);
+
+        // adding 2 to each element of the list.
         List<Integer> listPlus2 = list.stream().map( a ->  a+2  ).collect(Collectors.toList());
         System.out.println("listPlus2 = " + listPlus2);
 
